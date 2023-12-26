16ace8da024d377aedc3a6992fcfa427ac8674d0
JAEKWON0316 <149368895+JAEKWON0316@users.noreply.github.com>
2023-12-26 17:28:31 +0900

    Add files via upload

---
+++ first/23-12-26/script/001.js
@@ -0,0 +1,8 @@
+console.log("Hello JavaScript1");
+console.log("Hello JavaScript2");
+console.log("Hello JavaScript3");
+console.log("Hello JavaScript4");
+console.log("Hello JavaScript5");
+console.log("Hello JavaScript6");
+// 한 줄 주석 
+/* 여러줄 주석 */
