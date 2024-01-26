44bf4aa086bf35d99e0faeb19694911ca35589f5
JK Lee <149368895+JAEKWON0316@users.noreply.github.com>
2024-01-26 14:14:54 +0900

    Add files via upload

--- 24-01-22/js/script.js
+++ 24-01-22/js/script.js
@@ -6,9 +6,9 @@
 function swift(a){
 let b = a.substring(0,1);
 let c = a.substring(a.length-1,a.length);
-  console.log(c + a.substring(1,a.length-1) + b);
+  return (c + a.substring(1,a.length-1) + b);  //return을 해줄 때는 함수 밖에서 콘솔로 찍어줘야한다 리턴값을
 }
-swift("swift");
+console.log(swift("swift"));
 // 2. 양의 정수 x가 3또는 7의 배수인지 확인하는 함수를 작성하라.
 function check(x){
     if(x % 3 == 0 || x % 7 == 0){
@@ -25,7 +25,7 @@ function str(str1){
      let a = str1.substr(str1.length-3,3);
      console.log(a + str1 + a);
 }
-str("swift");
+str("swift");  //함수안에 콘솔로그가 있을 땐 그냥 함수에 매개변수만 넣어줘도 된다.
 
 // 4. 문자열이 'java'로 시작하는지 확인하라. (단, mach, search, indexOf같은 검색함수는 사용할 수 없다.
 function java(str2){
@@ -41,7 +41,7 @@ function java(str2){
 java("javascript");
 
 
-// 5. 임의의 수 x, y가 보두 50~99사이에 있는지 확인하라.
+// 5. 임의의 수 x, y가 모두 50~99사이에 있는지 확인하라.
 function check2(x,y){
     if(x >= 50 && x <= 99 && y >=50 && y <= 99){
         console.log("두 수가 50~99 사이에 있습니다");
